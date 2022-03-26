31a8c19f8daf89e644e213eb62d53188ddb6b3e4
Michelle McLean <lennasuzaka1988@gmail.com>
2022-03-26 17:04:37 -0500

    Automate closing pop-up box after search on site. Initialize Beautiful Soup and scrape price

--- Grocery Calculation.py
+++ Grocery Calculation.py
@@ -10,6 +10,7 @@ from selenium.webdriver.support.select import Select
 import time
 
 
+
 # try:
 #     grocery_list = pd.read_excel('G:/Sync/Shared Folder/Groceries and Takeout/Grocery List2.xlsx')
 # except FileNotFoundError:
@@ -25,6 +26,7 @@ import time
 #     return bill_with_dollar_sign
 
 
+# Initializing Selenium webdriver with a few options
 options = webdriver.ChromeOptions()
 options.add_argument('start-maximized')
 options.add_experimental_option('excludeSwitches', ['enable-automation'])
@@ -32,11 +34,30 @@ options.add_argument('--enable-javascript')
 options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=options)
 
-wait = WebDriverWait(driver, 10)
 
+# Heading to OG page and inputting search
 driver.get('https://www.sprouts.com/')
-driver.find_element(By.XPATH, "//ul[@id='ubermenu-nav-main-4-top-bar-r']//div[@role='button']").click()
+time.sleep(3)
+driver.find_element(By.XPATH, '//ul[@id="ubermenu-nav-main-4-top-bar-r"]//div[@role="button"]').click()
+driver.find_element(By.XPATH, "//div[@id='secondary-navigation']/div[@role='navigation']/ul[@class='ubermenu-nav']/li[3]//input[@name='nav-search']").send_keys('corn' + Keys.ENTER)
+current_page = driver.current_url
 
 
+# Automation for the search results page
+driver.get(current_page)
+time.sleep(5)
+driver.find_element(By.XPATH, '/html//button[@id="shopping-selector-parent-process-modal-close-click"]').click()
 
+
+# Initializing Beautiful Soup and requesting the page
+time.sleep(5)
+html = driver.page_source
+soup = BeautifulSoup(html, 'html5lib')
+soup.prettify()
+price = soup.select('li:nth-of-type(6)> div > div:nth-of-type(4)> div:nth-of-type(3) > div > div > react-product-price > div > div:nth-of-type(2) > span > span:nth-of-type(1)')
+for i in price:
+    print(i.text)
+
+
+# MAJOR NOTE: SOMETIMES COUPON ALERT POPS ON FRONT PAGE, FIND A WAY TO CLICK "MAYBE LATER" ON CONDITIONS
 # Label Sprouts, if in Sprouts column, detect images from Sprouts store only
