89792d54034bf88964563bb829813147bcec7859
lennasuzaka1988 <42519333+lennasuzaka1988@users.noreply.github.com>
2022-07-01 16:22:32 -0500

    Create test for resolution

---
+++ test.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from urllib.parse import urlparse
+import time
+
+
+
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('start-maximized')
+options.add_experimental_option('excludeSwitches', ['enable-automation'])
+options.add_argument('--disable-popup-blocking')
+options.add_argument('--enable-javascript')
+options.add_argument('--disable-notifications')
+options.add_experimental_option('detach', True)
+driver = webdriver.Chrome(options=options)
+
+
+driver.get('https://shop.sprouts.com/search?search_term=apple')
+
+
+def modal_close_out():
+    time.sleep(5)
+    shopping_selector_wait = WebDriverWait(driver, timeout=30).until(EC.visibility_of_element_located((
+        By.XPATH, '/html//button[@id="shopping-selector-parent-process-modal-close-click"]')))
+    shopping_selector_wait.click()
+    time.sleep(5)
+    scraping_price()
+
+
+def scraping_price():
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html5lib')
+    soup.prettify()
+    return soup.select('.css-f85de')
+        # print(elem.get('title'))
+
+
+print(modal_close_out())
+
