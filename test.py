9de0e217f1bde53d824bcdd246fd8d74d5d19c6d
Michelle <lennasuzaka1988@gmail.com>
2022-07-16 23:31:06 -0500

    Find alt route for fetching the correct price of product

--- test.py
+++ test.py
@@ -1,52 +1,56 @@
-# from bs4 import BeautifulSoup, BeautifulStoneSoup
-# from selenium import webdriver
-# from selenium.common.exceptions import TimeoutException
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.action_chains import ActionChains
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from urllib.parse import urlparse
-# import time
+from bs4 import BeautifulSoup, BeautifulStoneSoup
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from urllib.parse import urlparse
+import time
 import re
-#
-# options = webdriver.ChromeOptions()
-# options.add_argument('start-maximized')
-# options.add_experimental_option('excludeSwitches', ['enable-automation'])
-# options.add_argument('--disable-popup-blocking')
-# options.add_argument('--enable-javascript')
-# options.add_argument('--disable-notifications')
-# options.add_experimental_option('detach', True)
-# driver = webdriver.Chrome(options=options)
 
-# driver.get('https://shop.sprouts.com/search?search_term=apple')
+options = webdriver.ChromeOptions()
+options.add_argument('start-maximized')
+options.add_experimental_option('excludeSwitches', ['enable-automation'])
+options.add_argument('--disable-popup-blocking')
+options.add_argument('--enable-javascript')
+options.add_argument('--disable-notifications')
+options.add_experimental_option('detach', True)
+driver = webdriver.Chrome(options=options)
 
-
-# def modal_close_out():
-#     time.sleep(5)
-#     shopping_selector_wait = WebDriverWait(driver, timeout=30).until(EC.visibility_of_element_located((
-#         By.XPATH, '/html//button[@id="shopping-selector-parent-process-modal-close-click"]')))
-#     shopping_selector_wait.click()
-#     time.sleep(5)
+driver.get('https://shop.sprouts.com/search?search_term=apple')
 
 
-# bsoup = BeautifulSoup(driver.page_source, 'html.parser')
-# bsoup.prettify()
+def modal_close_out():
+    time.sleep(5)
+    shopping_selector_wait = WebDriverWait(driver, timeout=30).until(EC.visibility_of_element_located((
+        By.XPATH, '/html//button[@id="shopping-selector-parent-process-modal-close-click"]')))
+    shopping_selector_wait.click()
+    time.sleep(5)
 
 
 def closest_product_result(product_name, soup):
-    product_input_list = []
-    product_input_price_list = []
+    # product_input_list = []
+    # product_input_price_list = []
     product_input = soup.find(string=re.compile(product_name))
-    product_input_list.append(product_input)
-    product_input_price_list.append(
-        product_input.find_parent().find_parent().find('div', class_='css-1kh7mkb').text.strip('/lb'))
-    return product_input_list, product_input_price_list
+    # parent = product_input.find_parent().find_parent().find_parent().find_parent().find('react-product-price').find('span', class_='css-dr3s47').get_text()
+    for parent in product_input.parents:
+        print(parent.find('react-product-price'))
+    # return product_input
+    # product_input_list.append(product_input)
+    # product_input_price_list.append(
+    #     product_input.find_parent())
 
-#   assert find css-1kh7mb and print result
+    # return product_input_list, product_input_price_list
+
+
+# def closest_product_result_alt()
+
 
 # Find the price next to product input
-# modal_close_out()
-# time.sleep(5)
-# closest_product_result('Gala Apple')
-# driver.quit()
+modal_close_out()
+time.sleep(5)
+bsoup = BeautifulSoup(driver.page_source, 'html.parser')
+bsoup.prettify()
+print(closest_product_result('Gala Apple', bsoup))
