9be261f42c9c389602fbff747180e3d548cfd653
Michelle <lennasuzaka1988@gmail.com>
2022-08-09 23:24:24 -0500

    Attempt Selenium-based solution to same issue

--- tests/additional_test.py
+++ tests/additional_test.py
@@ -17,29 +17,30 @@ df_sprouts = pd.DataFrame(sprouts_grocery_list)
 sprouts_items = pd.DataFrame(sprouts_grocery_list, columns=['Item', 'Price'])
 
 
-# def first_item_search():
-#     product = import_grocery_list.df_sprouts.iat[0, 1]
-#     first_product_name_input(product)
-#     return closest_product_result(product)
+def first_item_search():
+    product = import_grocery_list.df_sprouts.iat[0, 1]
+    first_product_name_input(product)
+    return closest_product_result(product)
 
 
 def following_item_search():
     for index, row in sprouts_items.items():
         for x in row.dropna()[1:]:
+            clear_search()
             following_product_names_input(x)
-            # clear_search()
-            # following_product_names_input(x)
-            # current_page = driver.current_url
-            # driver.get(current_page)
+            time.sleep(10)
+            current_page = driver.current_url
+            driver.get(current_page)
+            result = driver.find_element(By.XPATH, '//div[text()="%s"]' % x)
+            print(result)
             # time.sleep(5)
             # bsoup = BeautifulSoup(driver.page_source, features='html5lib')
             # bsoup.prettify()
             # for product in bsoup.find_all(x):
             #     return product
-#
+
+
 store_navigation('64154')
-modal_close_out()
-# first_item_search()
-# time.sleep(10)
-print(following_item_search())
-# driver.quit()
+first_item_search()
+following_item_search()
+driver.quit()
